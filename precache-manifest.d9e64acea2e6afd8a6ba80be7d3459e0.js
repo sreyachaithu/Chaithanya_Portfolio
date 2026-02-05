693f121afb7c5a31cd418269980f172225c6b743
abc@gmail.com <chaithanyabommanaveni@gmail.com>
2026-02-05 15:36:51 +0530

    Updates

---
+++ precache-manifest.d9e64acea2e6afd8a6ba80be7d3459e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565e9e7dba5df690229779970d720e14",
+    "url": "/Chaithanya_Portfolio/index.html"
+  },
+  {
+    "revision": "4308b010a1109cfa9a07",
+    "url": "/Chaithanya_Portfolio/static/css/main.ebde3a0a.chunk.css"
+  },
+  {
+    "revision": "f252b0f37041f32f5bcd",
+    "url": "/Chaithanya_Portfolio/static/js/2.895af72f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Chaithanya_Portfolio/static/js/2.895af72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4308b010a1109cfa9a07",
+    "url": "/Chaithanya_Portfolio/static/js/main.644645fc.chunk.js"
+  },
+  {
+    "revision": "2d2a965247c8a4fefbe6",
+    "url": "/Chaithanya_Portfolio/static/js/runtime-main.cf353c68.js"
+  },
+  {
+    "revision": "395df93f3a2d8a841031e95505e05fa4",
+    "url": "/Chaithanya_Portfolio/static/media/header-background.395df93f.jpg"
+  }
+]);
